d284c930c9c2108e530e0c7ac47b69818930e2eb
Developer <kaungmyatsoe.m192@gmail.com>
2025-09-28 15:55:40 +0630

    feat: applicant

---
+++ Modules/Recruitment/Entities/JobFunction.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Recruitment\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JobFunction extends Model
+{
+    protected $fillable = [];
+}
--- Modules/Recruitment/Entities/JobPosting.php
+++ Modules/Recruitment/Entities/JobPosting.php
@@ -3,8 +3,20 @@
 namespace Modules\Recruitment\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class JobPosting extends Model
 {
-    protected $fillable = [];
+   use HasSlug;
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
