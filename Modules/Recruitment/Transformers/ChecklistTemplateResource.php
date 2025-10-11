eba09609ed51a690f40017036849f1eb78a926b5
Ye Myat Sandi Oo <yemyatsandi@gmail.com>
2025-10-11 11:23:01 +0630

    fix:checklist template

--- Modules/Recruitment/Transformers/ChecklistTemplateResource.php
+++ Modules/Recruitment/Transformers/ChecklistTemplateResource.php
@@ -2,7 +2,7 @@
 namespace Modules\Recruitment\Transformers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use Modules\Employee\App\Http\Resources\ChecklistTemplateItemResource;
+use Modules\Employee\App\Http\Resources\ChecklistTemplateItemResource; 
 
 class ChecklistTemplateResource extends JsonResource
 {
