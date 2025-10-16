71e15ba230d923052ceb0489d19779f3a9c3ad2b
Developer <developer@gmail.com>
2025-10-16 23:59:37 +0630

    remove: Assessment/Testing

--- Modules/Recruitment/App/Helpers/RecruitmentHelper.php
+++ Modules/Recruitment/App/Helpers/RecruitmentHelper.php
@@ -7,21 +7,21 @@ class RecruitmentHelper
 {
     public static function getJobApplicationActions($job_application, $current_status)
     {
-        $extract_data_action        = false;
-        $markAsReceived_action     = false;
-        $review_action             = false;
-        $assign_review_action      = false;
-        $shortlist_action          = false;
-        $accessment_testing_action = false;
-        $interview_action          = false;
-        $create_interview_action   = false;
-        $evaluation_action         = false;
-        $reference_check_action    = false;
-        $update_to_offer_action    = false;
-        $create_offer_action       = false;
-        $view_offer_action         = false;
-        $onboard_check_action      = false;
-        $create_employee           = false;
+        $extract_data_action   = false;
+        $markAsReceived_action = false;
+        $review_action         = false;
+        $assign_review_action  = false;
+        $shortlist_action      = false;
+        // $accessment_testing_action = false;
+        $interview_action        = false;
+        $create_interview_action = false;
+        $evaluation_action       = false;
+        $reference_check_action  = false;
+        $update_to_offer_action  = false;
+        $create_offer_action     = false;
+        $view_offer_action       = false;
+        $onboard_check_action    = false;
+        $create_employee         = false;
 
         if ($job_application->extractedData == null) {
             $extract_data_action = true;
@@ -35,8 +35,8 @@ class RecruitmentHelper
             $assign_review_action = true;
             $shortlist_action     = true;
         } else if ($current_status == RecruitmentStageTypes::SHORTLISTING->value) {
-            $accessment_testing_action = true;
-            $interview_action          = true;
+            // $accessment_testing_action = true;
+            $interview_action = true;
         } else if ($current_status == RecruitmentStageTypes::ASSESSMENT_TESTING->value) {
             $interview_action = true;
         } else if ($current_status == RecruitmentStageTypes::INTERVIEW->value) {
@@ -56,21 +56,21 @@ class RecruitmentHelper
         }
 
         return [
-            'extract_data_action'        => $extract_data_action,
-            'markAsReceived_action'     => $markAsReceived_action,
-            'review_action'             => $review_action,
-            'assign_review_action'      => $assign_review_action,
-            'shortlist_action'          => $shortlist_action,
-            'accessment_testing_action' => $accessment_testing_action,
-            'interview_action'          => $interview_action,
-            'create_interview_action'   => $create_interview_action,
-            'evaluation_action'         => $evaluation_action,
-            'reference_check_action'    => $reference_check_action,
-            'update_to_offer_action'    => $update_to_offer_action,
-            'create_offer_action'       => $create_offer_action,
-            'view_offer_action'         => $view_offer_action,
-            'onboard_check_action'      => $onboard_check_action,
-            'create_employee'           => $create_employee,
+            'extract_data_action'     => $extract_data_action,
+            'markAsReceived_action'   => $markAsReceived_action,
+            'review_action'           => $review_action,
+            'assign_review_action'    => $assign_review_action,
+            'shortlist_action'        => $shortlist_action,
+            // 'accessment_testing_action' => $accessment_testing_action,
+            'interview_action'        => $interview_action,
+            'create_interview_action' => $create_interview_action,
+            'evaluation_action'       => $evaluation_action,
+            'reference_check_action'  => $reference_check_action,
+            'update_to_offer_action'  => $update_to_offer_action,
+            'create_offer_action'     => $create_offer_action,
+            'view_offer_action'       => $view_offer_action,
+            'onboard_check_action'    => $onboard_check_action,
+            'create_employee'         => $create_employee,
         ];
     }
 }
