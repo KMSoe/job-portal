f23f5b4d8c214b9bcae55b476d27be896f822eb1
Ye Myat Sandi Oo <yemyatsandi@gmail.com>
2025-10-17 09:46:16 +0630

    fix:add timezone in job interview

--- Modules/Recruitment/Database/Migrations/2025_10_17_031000_add_timezone_id_in_job_application_interviews_table.php
+++ Modules/Recruitment/Database/Migrations/2025_10_17_031000_add_timezone_id_in_job_application_interviews_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddCurrencyIdInJobApplicationInterviewsTable extends Migration
+class AddTimezoneIdInJobApplicationInterviewsTable extends Migration
 {
     /**
      * Run the migrations.
