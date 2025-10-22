4c19a6f305901b94991dfc078c497f30d6b0be49
Developer <developer@gmail.com>
2025-10-22 22:04:05 +0630

    fix

--- Modules/Recruitment/Routes/api.php
+++ Modules/Recruitment/Routes/api.php
@@ -86,7 +86,7 @@ Route::middleware(['auth:api'])->prefix('/v1/recruitment')->group(function () {
     Route::get('job-posting-templates-page-data', [JobPostingTemplateController::class, 'getPageData']);
     Route::resource('job-postings', JobPostingController::class);
     Route::get('job-posting-sample-download', [JobPostingController::class, 'downloadSampleExcelFile']);
-    Route::post('job-posting/import', [JobPostingController::class, 'import'])->name('skills.import');
+    Route::post('job-posting/import', [JobPostingController::class, 'import'])->name('job_postings.import');
     Route::get('job-postings-page-data', [JobPostingController::class, 'getPageData']);
     Route::get('job-postings-detail-page-data', [JobApplicationBoardController::class, 'getPageData']);
     Route::get('job-postings/{job_posting_id}/applicants', [JobApplicationBoardController::class, 'getApplicants']);
